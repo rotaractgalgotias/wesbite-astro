95a053c962cc4e37348493ad0b4447b07596a081
areeburrub <areeburrub@gmail.com>
2023-06-11 18:49:56 +0530

    added: sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,5 +7,6 @@ import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://rotaractgalgotias.org',
   integrations: [tailwind(), react(), mdx(), sitemap()]
 });
