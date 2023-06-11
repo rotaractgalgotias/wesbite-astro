b01a95fae3e8f6c61f0db4457d4e20ea6ffe2287
areeburrub <areeburrub@gmail.com>
2023-06-11 19:47:37 +0530

    debug: gallery grid not loading

--- src/components/event-gallery.tsx
+++ src/components/event-gallery.tsx
@@ -2,45 +2,45 @@ import {useEffect, useState} from "react";
 import Gallery from "react-photo-gallery";
 
 const EventGallery = ({ images, event }) => {
-  async function getPhotosWithAspectRatio(files) {
-    const images = files.map(file => {
-      return new Promise((resolve) => {
-        const img = new Image();
-        img.src = file;
-
-        img.onload = () => {
-          const width = img.naturalWidth;
-          const height = img.naturalHeight;
-
-          resolve({
-            src: file,
-            width: width / height,
-            height: 1,
-          });
-        };
-      });
-    });
-
-    return Promise.all(images);
-  }
-
-  const getPhotos = async () => {
-    return await getPhotosWithAspectRatio(images);
-  };
-
-  const [photos, setPhotos] = useState([]);
-
-  useEffect(() => {
-    getPhotos().then((res) => {
-      console.log(res);
-      setPhotos(res);
-    });
-  }, []);
+  // async function getPhotosWithAspectRatio(files) {
+  //   const images = files.map(file => {
+  //     return new Promise((resolve) => {
+  //       const img = new Image();
+  //       img.src = file;
+  //
+  //       img.onload = () => {
+  //         const width = img.naturalWidth;
+  //         const height = img.naturalHeight;
+  //
+  //         resolve({
+  //           src: file,
+  //           width: width / height,
+  //           height: 1,
+  //         });
+  //       };
+  //     });
+  //   });
+  //
+  //   return Promise.all(images);
+  // }
+  //
+  // const getPhotos = async () => {
+  //   return await getPhotosWithAspectRatio(images);
+  // };
+  //
+  // const [photos, setPhotos] = useState([]);
+  //
+  // useEffect(() => {
+  //   getPhotos().then((res) => {
+  //     console.log(res);
+  //     setPhotos(res);
+  //   });
+  // }, []);
 
   // @ts-ignore
   return (
     <div className={"flex flex-col gap-3 mx-auto my-4"}>
-      <Gallery photos={photos} />
+      <Gallery photos={images} />
     </div>
   );
 };
