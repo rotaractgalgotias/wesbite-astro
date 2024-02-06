cd0b0cb56f280329d1462abeabb5374a7b9cea9a
Rishi Raj Jain <rishi18304@iiitd.ac.in>
2024-02-06 22:50:25 +0530

    feat: astro-font - optimize fonts & prevent CLS

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,16 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {
-			fontFamily: {
-				dmSans: ["DM Sans", "sans-serif"],
-				outfit: ["Outfit Variable", "sans-serif"],
-			},
-		},
-	},
-	plugins: [
-		require('prettier-plugin-tailwindcss'),
-		require('@tailwindcss/typography'),
-	],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        dmSans: ["var(--dm-sans)"],
+        outfit: ["var(--outfit-variable)"],
+      },
+    },
+  },
+  plugins: [
+    require("prettier-plugin-tailwindcss"),
+    require("@tailwindcss/typography"),
+  ],
+};
