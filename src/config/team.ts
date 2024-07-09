bf16216092796e0c6bcba90cd7c8e1796b6a1f51
c-w-d-harshit <cwd.harshit911@gmail.com>
2024-07-09 18:20:56 +0530

    waiting for 2 more images

--- src/config/team.ts
+++ src/config/team.ts
@@ -1,10 +1,10 @@
-interface SubGroup{
+interface SubGroup {
   name: string;
-  members : {
-    id:string,
-    fullName : string,
-    mdxFile : string
-  }[]
+  members: {
+    id: string;
+    fullName: string;
+    mdxFile: string;
+  }[];
 }
 
 interface Team {
@@ -13,283 +13,272 @@ interface Team {
   subGroups: SubGroup[];
 }
 
-export const TeamData:Team[] = [
+export const TeamData: Team[] = [
   {
-    id:"council",
-    name:"Board Council",
-    subGroups:[
+    id: "council",
+    name: "Board Council",
+    subGroups: [
       {
-        name:"President",
-        members:[
+        name: "President",
+        members: [
           {
-            id:"01",
-            fullName:"Ashish Kumar Gupta",
-            mdxFile:""
-          }
-        ]
+            id: "01",
+            fullName: "Divyanshu Katiyar",
+            mdxFile: "",
+          },
+        ],
       },
       {
-        name:"Secretary",
-        members:[
+        name: "Secretary",
+        members: [
           {
-            id:"01",
-            fullName:"Unnati Agarwal",
-            mdxFile:""
-          }
-        ]
+            id: "01",
+            fullName: "Unnati Agarwal",
+            mdxFile: "",
+          },
+        ],
       },
       {
-        name:"Vice President",
-        members:[
+        name: "Vice President",
+        members: [
           {
-            id:"01",
-            fullName:"Aditya Kumar Ojha",
-            mdxFile:""
-          }
-        ]
+            id: "01",
+            fullName: "Anurag Kaushik",
+            mdxFile: "",
+          },
+        ],
       },
       {
-        name:"Joint Secretary",
-        members:[
+        name: "Public Relation Officer",
+        members: [
           {
-            id:"01",
-            fullName:"Yashica Chauhan",
-            mdxFile:""
-          }
-        ]
+            id: "01",
+            fullName: "Sanket Gupta",
+            mdxFile: "",
+          },
+        ],
       },
       {
-        name:"Treasurer",
-        members:[
+        name: "Sergeant-at-Arms",
+        members: [
           {
-            id:"01",
-            fullName:"Indu Banerjee",
-            mdxFile:""
-          }
-        ]
+            id: "01",
+            fullName: "Yash Gupta",
+            mdxFile: "",
+          },
+        ],
       },
       {
-        name:"Sergeant-at-Arms",
-        members:[
+        name: "Joint Secretary",
+        members: [
           {
-            id:"01",
-            fullName:"Rohit Saini",
-            mdxFile:""
-          }
-        ]
+            id: "01",
+            fullName: "Ritik Varshney",
+            mdxFile: "",
+          },
+        ],
       },
-      {
-        name:"Aid-to-President",
-        members:[
-          {
-            id:"01",
-            fullName:"Aadarsh Rai",
-            mdxFile:""
-          }
-        ]
-      },
-    ]
+    ],
   },
   {
-    id:"directors",
-    name:"Board of Directors",
-    subGroups:[
+    id: "directors",
+    name: "Board of Directors",
+    subGroups: [
       {
-        name:"Club Service",
-        members:[
+        name: "Club Service",
+        members: [
           {
-            id:"D01",
-            fullName:"Ananya Gupta",
-            mdxFile:""
+            id: "D01",
+            fullName: "Ananya Gupta",
+            mdxFile: "",
           },
           {
-            id:"D02",
-            fullName:"Prachi Singh",
-            mdxFile:""
-          }
-        ]
+            id: "D02",
+            fullName: "Prachi Singh",
+            mdxFile: "",
+          },
+        ],
       },
       {
-        name:"Community Service",
-        members:[
+        name: "Community Service",
+        members: [
           {
-            id:"D03",
-            fullName:"Dhruv Jha",
-            mdxFile:""
+            id: "D03",
+            fullName: "Dhruv Jha",
+            mdxFile: "",
           },
           {
-            id:"D04",
-            fullName:"Priyesh Singh",
-            mdxFile:""
+            id: "D04",
+            fullName: "Priyesh Singh",
+            mdxFile: "",
           },
           {
-            id:"D05",
-            fullName:"Shubham Pandey",
-            mdxFile:""
-          }
-        ]
+            id: "D05",
+            fullName: "Shubham Pandey",
+            mdxFile: "",
+          },
+        ],
       },
       {
-        name:"Vocational Service",
-        members:[
+        name: "Vocational Service",
+        members: [
           {
-            id:"D06",
-            fullName:"Kritika Singh",
-            mdxFile:""
+            id: "D06",
+            fullName: "Kritika Singh",
+            mdxFile: "",
           },
           {
-            id:"D07",
-            fullName:"Aditya Garg",
-            mdxFile:""
-          }
-        ]
+            id: "D07",
+            fullName: "Aditya Garg",
+            mdxFile: "",
+          },
+        ],
       },
       {
-        name:"International Service",
-        members:[
+        name: "International Service",
+        members: [
           {
-            id:"D08",
-            fullName:"Aman Wadhwani",
-            mdxFile:""
+            id: "D08",
+            fullName: "Aman Wadhwani",
+            mdxFile: "",
           },
           {
-            id:"D09",
-            fullName:"Sanket Gupta",
-            mdxFile:""
+            id: "D09",
+            fullName: "Sanket Gupta",
+            mdxFile: "",
           },
           {
-            id:"D010",
-            fullName:"Shivendra Pratap Singh",
-            mdxFile:""
-          }
-        ]
+            id: "D010",
+            fullName: "Shivendra Pratap Singh",
+            mdxFile: "",
+          },
+        ],
       },
       {
-        name:"Literary Service",
-        members:[
+        name: "Literary Service",
+        members: [
           {
-            id:"D11",
-            fullName:"Shrishti Shrivastava",
-            mdxFile: ""
+            id: "D11",
+            fullName: "Shrishti Shrivastava",
+            mdxFile: "",
           },
           {
-            id:"D12",
-            fullName:"Shivansh Nigam",
-            mdxFile:""
-          }
-        ]
+            id: "D12",
+            fullName: "Shivansh Nigam",
+            mdxFile: "",
+          },
+        ],
       },
       {
-        name:"Multimedia Service",
-        members:[
+        name: "Multimedia Service",
+        members: [
           {
-            id:"D18",
-            fullName:"Akansha Choudhary",
-            mdxFile:""
+            id: "D18",
+            fullName: "Akansha Choudhary",
+            mdxFile: "",
           },
           {
-            id:"D19",
-            fullName:"Pranav Raj",
-            mdxFile:""
+            id: "D19",
+            fullName: "Pranav Raj",
+            mdxFile: "",
           },
           {
-            id:"D20",
-            fullName:"Anirudh Singh",
-            mdxFile:""
-          }
-        ]
+            id: "D20",
+            fullName: "Anirudh Singh",
+            mdxFile: "",
+          },
+        ],
       },
       {
-        name:"Public Relation Services",
-        members:[
+        name: "Public Relation Services",
+        members: [
           {
-            id:"D13",
-            fullName:"Devansh Shrivasava",
-            mdxFile:""
+            id: "D13",
+            fullName: "Devansh Shrivasava",
+            mdxFile: "",
           },
           {
-            id:"D15",
-            fullName:"Sanskar Jaiswal",
-            mdxFile:""
+            id: "D15",
+            fullName: "Sanskar Jaiswal",
+            mdxFile: "",
           },
           {
-            id:"D15",
-            fullName:"Vikas Singh",
-            mdxFile:""
-          }
-        ]
+            id: "D15",
+            fullName: "Vikas Singh",
+            mdxFile: "",
+          },
+        ],
       },
       {
-        name:"Photography Service",
-        members:[
+        name: "Photography Service",
+        members: [
           {
-            id:"D16",
-            fullName:"Shreyash Srivastava",
-            mdxFile:""
+            id: "D16",
+            fullName: "Shreyash Srivastava",
+            mdxFile: "",
           },
           {
-            id:"D17",
-            fullName:"Rachit Katiyar",
-            mdxFile:""
-          }
-        ]
+            id: "D17",
+            fullName: "Rachit Katiyar",
+            mdxFile: "",
+          },
+        ],
       },
       {
-        name:"Social Media",
-        members:[
+        name: "Social Media",
+        members: [
           {
-            id:"D21",
-            fullName:"Urvashi Bajpai",
-            mdxFile:""
+            id: "D21",
+            fullName: "Urvashi Bajpai",
+            mdxFile: "",
           },
           {
-            id:"D22",
-            fullName:"Aakanshi Garg",
-            mdxFile:""
-          }
-        ]
+            id: "D22",
+            fullName: "Aakanshi Garg",
+            mdxFile: "",
+          },
+        ],
       },
       {
-        name:"Management Team Head",
-        members:[
+        name: "Management Team Head",
+        members: [
           {
-            id:"D21",
-            fullName:"Krishna Samrat Bajpai",
-            mdxFile:""
+            id: "D21",
+            fullName: "Krishna Samrat Bajpai",
+            mdxFile: "",
           },
           {
-            id:"D12",
-            fullName:"Abhishek Singh",
-            mdxFile:""
+            id: "D12",
+            fullName: "Abhishek Singh",
+            mdxFile: "",
           },
-          
-        ]
+        ],
       },
       {
-        name:"Performing Arts Head",
-        members:[
+        name: "Performing Arts Head",
+        members: [
           {
-            id:"D21",
-            fullName:"Samridh Srivastava",
-            mdxFile:""
+            id: "D21",
+            fullName: "Samridh Srivastava",
+            mdxFile: "",
           },
           {
-            id:"D23",
-            fullName:"Nandani Mittal",
-            mdxFile:""
-          }          
-        ]
+            id: "D23",
+            fullName: "Nandani Mittal",
+            mdxFile: "",
+          },
+        ],
       },
       {
-        name:"Techincal Services",
-        members:[
+        name: "Techincal Services",
+        members: [
           {
-            id:"D24",
-            fullName:"Utkarsh Khare",
-            mdxFile:""
-          }
-        ]
-      }
-    ]
-  }
-  ]
+            id: "D24",
+            fullName: "Utkarsh Khare",
+            mdxFile: "",
+          },
+        ],
+      },
+    ],
+  },
+];
